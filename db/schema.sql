b0e7ba78d62268783df11c9877e935796ede661c
unknown <parkerosemeyer@gmail.com>
2024-05-22 16:48:32 -0500

    Added info in seeds to put in tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,31 @@
-DROP DATABASE IF EXISTS department;
+DROP DATABASE IF EXISTS employee_db;
 
-DROP DATABASE IF EXISTS employee_role;
+CREATE DATABASE employee_db;
 
-DROP DATABASE IF EXISTS employee;
+\c employee_db;
 
-CREATE DATABASE department;
-CREATE DATABASE employee_role;
-CREATE DATABASE employee;
+CREATE TABLE department (
+    id SERIAL PRIMARY KEY,
+    "name" VARCHAR(30) UNIQUE NOT NULL
+);
+
+CREATE TABLE role (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30) UNIQUE NOT NULL,
+    last_name VARCHAR(30) UNIQUE NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER DEFAULT null,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL
+);
