772103b5d0bfd6eeead3dcffd893182df380b8e1
unknown <parkerosemeyer@gmail.com>
2024-05-29 16:04:28 -0500

    fixed null as manager

--- utils/role.js
+++ utils/role.js
@@ -1,1 +1,30 @@
-const inquirer = require('inquirer')
+const {addRole} = require('./inquirer.js')
+const { Pool } = require('pg')
+
+const pool = new Pool({
+    user: 'postgres',
+    password: 'password',
+    host: 'localhost',
+    database: 'employee_db'
+})
+async function addingRole(){
+        // Creates an array of the most current departments
+    const departments = await pool.query('SELECT name FROM department')
+    let departmentNames = []
+    for (let i = 0; i < departments.rows.length; i++) {
+        departmentNames.push(departments.rows[i].name)
+    }
+
+    const answers = await addRole(departmentNames)
+    // Inserts inquired role data into the role table
+    for (let i = 0; i < departments.rows.length; i++) {
+        if (departments.rows[i].name === answers.role_department) {
+            await pool.query(`
+            INSERT INTO role(title, salary, department_id) 
+            VALUES ('${answers.role_name}', '${answers.role_salary}', ${i + 1})
+            `)
+        }
+    }
+    console.log(`Added ${answers.role_name} to the database`)
+}
+module.exports = addingRole
---
+++ utils/update.js
@@ -0,0 +1,41 @@
+const {updateEmployee} = require('./inquirer.js')
+const { Pool } = require('pg')
+
+const pool = new Pool({
+    user: 'postgres',
+    password: 'password',
+    host: 'localhost',
+    database: 'employee_db'
+})
+
+async function updatingEmployee(){
+    // Creates an array of the most current employee names with both first and last names
+    const employeeNames = await pool.query('SELECT first_name, last_name FROM employee')
+    let names = []
+    for (let i = 0; i < employeeNames.rows.length; i++) {
+        names.push(`${employeeNames.rows[i].first_name} ${employeeNames.rows[i].last_name}`)
+    }
+    // Creates an array of the most current roles for employees
+    const workRoles = await pool.query('SELECT title FROM role')
+    let roles = []
+    for (let i = 0; i < workRoles.rows.length; i++) {
+        roles.push(workRoles.rows[i].title)
+    }
+    const answers = await updateEmployee(names, roles)
+    // First for loop helps convert the string answer for role to an integer
+    for (let i = 0; i < workRoles.rows.length; i++) {
+        if (workRoles.rows[i].title === answers.employee_role) {
+            // Second for loop helps check combined first and last name to the correct first name only
+            for (let x = 0; x < employeeNames.rows.length; x++) {
+                if (`${employeeNames.rows[x].first_name} ${employeeNames.rows[x].last_name}` === answers.employee_name) {
+                    await pool.query(`
+                    UPDATE employee SET role_id = ${i + 1} WHERE first_name = '${employeeNames.rows[x].first_name}'
+                `)
+                }
+            }
+        }
+    }
+    console.log(`Updated ${answers.employee_name} to the database`)
+}
+
+module.exports = updatingEmployee
